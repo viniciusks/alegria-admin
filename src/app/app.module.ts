e66c8b4dd021acd3a568b0ed8fca2beacafce0bb
Vinicius Kremer Santos <vinicius.kremer@gmail.com>
2023-02-13 21:40:47 -0300

    [AD-8] Subindo inicio de refatoração login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,10 @@ import { LoginComponent } from './components/login/login.component';
 import { AccordionModule } from 'primeng/accordion'; //accordion and accordion tab
 import { MenuItem } from 'primeng/api'; //api
 import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+
+// Libs
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent],
@@ -19,6 +24,9 @@ import { CardModule } from 'primeng/card';
     AppRoutingModule,
     CardModule,
     AccordionModule,
+    InputTextModule,
+    FormsModule,
+    NgxSpinnerModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  public validateForm: boolean;
+  public errorFlag: boolean;
 
+  constructor(private _router: Router, private _spinner: NgxSpinnerService) {
+    this.validateForm = true;
+    this.errorFlag = false;
+  }
+
+  onSubmit(form: any) {}
 }
