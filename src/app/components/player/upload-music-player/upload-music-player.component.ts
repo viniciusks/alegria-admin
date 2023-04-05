b67d981607b20588cf02509a87173171d2f058ff
Vinicius Kremer Santos <vinicius.kremer@gmail.com>
2023-04-04 22:37:14 -0300

    [AD-8] Adicionando upload-music-player e melhorando alguns detalhes de album

---
+++ src/app/components/player/upload-music-player/upload-music-player.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-music-player',
+  templateUrl: './upload-music-player.component.html',
+  styleUrls: ['./upload-music-player.component.css']
+})
+export class UploadMusicPlayerComponent {
+
+}
