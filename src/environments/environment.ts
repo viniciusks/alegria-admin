33163595d1dbbc270e856071ff3158f3d8721d9b
Vinicius Kremer Santos <vinicius.kremer@gmail.com>
2023-04-12 08:32:56 -0300

    [AD-3] Adicionando arquivos de deploy no firebase e arrumando url api player

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     apiUrl: 'http://my-prod-url',
-    apiUrlPlayer: 'https://api-player.alegriacrista-tech.com.br/'
+    apiUrlPlayer: 'https://api-player.alegriacrista-tech.com.br'
 };
